623c63e7b65f49605785b1deb252664afcc35b0f
Hongchan Choi <hongchan@chromium.org>
2015-02-17 15:42:54 -0800

    update

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp        = require('gulp'),
+    plugins     = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync'),
+    runSequence = require('run-sequence'),
+    del         = require('del');
+
+var deploy      = require('gulp-gh-pages');
+
+var reload      = browserSync.reload;
+
+
+gulp.task('serve', function () {
+  browserSync({
+    notify: false,
+    server: {
+      baseDir: './app'
+    },
+    browser: 'google chrome'
+    // browser: 'google chrome canary'
+  });
+
+  gulp.watch([
+    'app/**/*.html',
+    'app/**/*.js',
+    '!app/codemirror/**/*'
+  ], reload);
+});
+
+
+// gh-pages integration
+gulp.task('deploy', function () {
+  return gulp.src('app/**/*')
+    .pipe(deploy());
+});
+
+
+gulp.task('default', function (cb) {
+  runSequence('serve', cb);
+});
