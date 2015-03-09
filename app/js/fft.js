0260d962fcc258366ff2b74d240a2ff711f707c4
Hongchan Choi <hongchan.choi@gmail.com>
2015-03-09 16:49:07 -0700

    starting new branch

--- app/js/fft.js
+++ app/js/fft.js
@@ -1,225 +1,345 @@
-'use strict';
+// Fourier Transform Module used by DFT, FFT, RFFT
+function FourierTransform(bufferSize, sampleRate) {
+  this.bufferSize = bufferSize;
+  this.sampleRate = sampleRate;
+  this.bandwidth  = 2 / bufferSize * sampleRate / 2;
 
-!function(exports, complex_array) {
+  this.spectrum   = new Float32Array(bufferSize/2);
+  this.real       = new Float32Array(bufferSize);
+  this.imag       = new Float32Array(bufferSize);
 
-  var
-    ComplexArray = complex_array.ComplexArray,
-    // Math constants and functions we need.
-    PI = Math.PI,
-    SQRT1_2 = Math.SQRT1_2,
-    sqrt = Math.sqrt,
-    cos = Math.cos,
-    sin = Math.sin
+  this.peakBand   = 0;
+  this.peak       = 0;
 
-  ComplexArray.prototype.FFT = function() {
-    return FFT(this, false)
-  }
+  /**
+   * Calculates the *middle* frequency of an FFT band.
+   * @param {Number} index The index of the FFT band.
+   * @returns The middle frequency in Hz.
+   */
+  this.getBandFrequency = function(index) {
+    return this.bandwidth * index + this.bandwidth / 2;
+  };
 
-  exports.FFT = function(input) {
-    return ensureComplexArray(input).FFT()
-  }
+  this.calculateSpectrum = function() {
+    var spectrum  = this.spectrum,
+        real      = this.real,
+        imag      = this.imag,
+        bSi       = 2 / this.bufferSize,
+        sqrt      = Math.sqrt,
+        rval, 
+        ival,
+        mag;
 
-  ComplexArray.prototype.InvFFT = function() {
-    return FFT(this, true)
-  }
+    for (var i = 0, N = bufferSize/2; i < N; i++) {
+      rval = real[i];
+      ival = imag[i];
+      mag = bSi * sqrt(rval * rval + ival * ival);
 
-  exports.InvFFT = function(input) {
-    return ensureComplexArray(input).InvFFT()
-  }
-
-  // Applies a frequency-space filter to input, and returns the real-space
-  // filtered input.
-  // filterer accepts freq, i, n and modifies freq.real and freq.imag.
-  ComplexArray.prototype.frequencyMap = function(filterer) {
-    return this.FFT().map(filterer).InvFFT()
-  }
-
-  exports.frequencyMap = function(input, filterer) {
-    return ensureComplexArray(input).frequencyMap(filterer)
-  }
-
-  function ensureComplexArray(input) {
-    return complex_array.isComplexArray(input) && input ||
-        new ComplexArray(input)
-  }
-
-  function FFT(input, inverse) {
-    var n = input.length
-
-    if (n & (n - 1)) {
-      return FFT_Recursive(input, inverse)
-    } else {
-      return FFT_2_Iterative(input, inverse)
-    }
-  }
-
-  function FFT_Recursive(input, inverse) {
-    var
-      n = input.length,
-      // Counters.
-      i, j,
-      output,
-      // Complex multiplier and its delta.
-      f_r, f_i, del_f_r, del_f_i,
-      // Lowest divisor and remainder.
-      p, m,
-      normalisation,
-      recursive_result,
-      _swap, _real, _imag
-
-    if (n === 1) {
-      return input
-    }
-
-    output = new ComplexArray(n, input.ArrayType)
-
-    // Use the lowest odd factor, so we are able to use FFT_2_Iterative in the
-    // recursive transforms optimally.
-    p = LowestOddFactor(n)
-    m = n / p
-    normalisation = 1 / sqrt(p)
-    recursive_result = new ComplexArray(m, input.ArrayType)
-
-    // Loops go like O(n Σ p_i), where p_i are the prime factors of n.
-    // for a power of a prime, p, this reduces to O(n p log_p n)
-    for(j = 0; j < p; j++) {
-      for(i = 0; i < m; i++) {
-        recursive_result.real[i] = input.real[i * p + j]
-        recursive_result.imag[i] = input.imag[i * p + j]
-      }
-      // Don't go deeper unless necessary to save allocs.
-      if (m > 1) {
-        recursive_result = FFT(recursive_result, inverse)
+      if (mag > this.peak) {
+        this.peakBand = i;
+        this.peak = mag;
       }
 
-      del_f_r = cos(2*PI*j/n)
-      del_f_i = (inverse ? -1 : 1) * sin(2*PI*j/n)
-      f_r = 1
-      f_i = 0
+      spectrum[i] = mag;
+    }
+  };
+}
 
-      for(i = 0; i < n; i++) {
-        _real = recursive_result.real[i % m]
-        _imag = recursive_result.imag[i % m]
 
-        output.real[i] += f_r * _real - f_i * _imag
-        output.imag[i] += f_r * _imag + f_i * _real
+/**
+ * RFFT is a class for calculating the Discrete Fourier Transform of a signal
+ * with the Fast Fourier Transform algorithm.
+ *
+ * This method currently only contains a forward transform but is highly optimized.
+ *
+ * @param {Number} bufferSize The size of the sample buffer to be computed. Must be power of 2
+ * @param {Number} sampleRate The sampleRate of the buffer (eg. 44100)
+ *
+ * @constructor
+ */
 
-        _swap = f_r * del_f_r - f_i * del_f_i
-        f_i = f_r * del_f_i + f_i * del_f_r
-        f_r = _swap
+// lookup tables don't really gain us any speed, but they do increase
+// cache footprint, so don't use them in here
+
+// also we don't use sepearate arrays for real/imaginary parts
+
+// this one a little more than twice as fast as the one in FFT
+// however I only did the forward transform
+
+// the rest of this was translated from C, see http://www.jjj.de/fxt/
+// this is the real split radix FFT
+
+function RFFT(bufferSize, sampleRate) {
+  FourierTransform.call(this, bufferSize, sampleRate);
+
+  this.trans = new Float32Array(bufferSize);
+
+  this.reverseTable = new Uint32Array(bufferSize);
+
+  // don't use a lookup table to do the permute, use this instead
+  this.reverseBinPermute = function (dest, source) {
+    var bufferSize  = this.bufferSize, 
+        halfSize    = bufferSize >>> 1, 
+        nm1         = bufferSize - 1, 
+        i = 1, r = 0, h;
+
+    dest[0] = source[0];
+
+    do {
+      r += halfSize;
+      dest[i] = source[r];
+      dest[r] = source[i];
+      
+      i++;
+
+      h = halfSize << 1;
+      while (h = h >> 1, !((r ^= h) & h));
+
+      if (r >= i) { 
+        dest[i]     = source[r]; 
+        dest[r]     = source[i];
+
+        dest[nm1-i] = source[nm1-r]; 
+        dest[nm1-r] = source[nm1-i];
       }
-    }
+      i++;
+    } while (i < halfSize);
+    dest[nm1] = source[nm1];
+  };
 
-    // Copy back to input to match FFT_2_Iterative in-placeness
-    // TODO: faster way of making this in-place?
-    for(i = 0; i < n; i++) {
-      input.real[i] = normalisation * output.real[i]
-      input.imag[i] = normalisation * output.imag[i]
-    }
+  this.generateReverseTable = function () {
+    var bufferSize  = this.bufferSize, 
+        halfSize    = bufferSize >>> 1, 
+        nm1         = bufferSize - 1, 
+        i = 1, r = 0, h;
 
-    return input
+    this.reverseTable[0] = 0;
+
+    do {
+      r += halfSize;
+      
+      this.reverseTable[i] = r;
+      this.reverseTable[r] = i;
+
+      i++;
+
+      h = halfSize << 1;
+      while (h = h >> 1, !((r ^= h) & h));
+
+      if (r >= i) { 
+        this.reverseTable[i] = r;
+        this.reverseTable[r] = i;
+
+        this.reverseTable[nm1-i] = nm1-r;
+        this.reverseTable[nm1-r] = nm1-i;
+      }
+      i++;
+    } while (i < halfSize);
+
+    this.reverseTable[nm1] = nm1;
+  };
+
+  this.generateReverseTable();
+}
+
+
+// Ordering of output:
+//
+// trans[0]     = re[0] (==zero frequency, purely real)
+// trans[1]     = re[1]
+//             ...
+// trans[n/2-1] = re[n/2-1]
+// trans[n/2]   = re[n/2]    (==nyquist frequency, purely real)
+//
+// trans[n/2+1] = im[n/2-1]
+// trans[n/2+2] = im[n/2-2]
+//             ...
+// trans[n-1]   = im[1] 
+
+RFFT.prototype.forward = function(buffer) {
+  var n         = this.bufferSize, 
+      spectrum  = this.spectrum,
+      x         = this.trans, 
+      TWO_PI    = 2*Math.PI,
+      sqrt      = Math.sqrt,
+      i         = n >>> 1,
+      bSi       = 2 / n,
+      n2, n4, n8, nn, 
+      t1, t2, t3, t4, 
+      i1, i2, i3, i4, i5, i6, i7, i8, 
+      st1, cc1, ss1, cc3, ss3,
+      e, 
+      a,
+      rval, ival, mag; 
+
+  this.reverseBinPermute(x, buffer);
+
+  /*
+  var reverseTable = this.reverseTable;
+  for (var k = 0, len = reverseTable.length; k < len; k++) {
+    x[k] = buffer[reverseTable[k]];
+  }
+  */
+
+  for (var ix = 0, id = 4; ix < n; id *= 4) {
+    for (var i0 = ix; i0 < n; i0 += id) {
+      //sumdiff(x[i0], x[i0+1]); // {a, b}  <--| {a+b, a-b}
+      st1 = x[i0] - x[i0+1];
+      x[i0] += x[i0+1];
+      x[i0+1] = st1;
+    } 
+    ix = 2*(id-1);
   }
 
-  function FFT_2_Iterative(input, inverse) {
-    var
-      n = input.length,
-      // Counters.
-      i, j,
-      output, output_r, output_i,
-      // Complex multiplier and its delta.
-      f_r, f_i, del_f_r, del_f_i, temp,
-      // Temporary loop variables.
-      l_index, r_index,
-      left_r, left_i, right_r, right_i,
-      // width of each sub-array for which we're iteratively calculating FFT.
-      width
+  n2 = 2;
+  nn = n >>> 1;
 
-    output = BitReverseComplexArray(input)
-    output_r = output.real
-    output_i = output.imag
-    // Loops go like O(n log n):
-    //   width ~ log n; i,j ~ n
-    width = 1
-    while (width < n) {
-      del_f_r = cos(PI/width)
-      del_f_i = (inverse ? -1 : 1) * sin(PI/width)
-      for (i = 0; i < n/(2*width); i++) {
-        f_r = 1
-        f_i = 0
-        for (j = 0; j < width; j++) {
-          l_index = 2*i*width + j
-          r_index = l_index + width
-
-          left_r = output_r[l_index]
-          left_i = output_i[l_index]
-          right_r = f_r * output_r[r_index] - f_i * output_i[r_index]
-          right_i = f_i * output_r[r_index] + f_r * output_i[r_index]
-
-          output_r[l_index] = SQRT1_2 * (left_r + right_r)
-          output_i[l_index] = SQRT1_2 * (left_i + right_i)
-          output_r[r_index] = SQRT1_2 * (left_r - right_r)
-          output_i[r_index] = SQRT1_2 * (left_i - right_i)
-          temp = f_r * del_f_r - f_i * del_f_i
-          f_i = f_r * del_f_i + f_i * del_f_r
-          f_r = temp
+  while((nn = nn >>> 1)) {
+    ix = 0;
+    n2 = n2 << 1;
+    id = n2 << 1;
+    n4 = n2 >>> 2;
+    n8 = n2 >>> 3;
+    do {
+      if(n4 !== 1) {
+        for(i0 = ix; i0 < n; i0 += id) {
+          i1 = i0;
+          i2 = i1 + n4;
+          i3 = i2 + n4;
+          i4 = i3 + n4;
+     
+          //diffsum3_r(x[i3], x[i4], t1); // {a, b, s} <--| {a, b-a, a+b}
+          t1 = x[i3] + x[i4];
+          x[i4] -= x[i3];
+          //sumdiff3(x[i1], t1, x[i3]);   // {a, b, d} <--| {a+b, b, a-b}
+          x[i3] = x[i1] - t1; 
+          x[i1] += t1;
+     
+          i1 += n8;
+          i2 += n8;
+          i3 += n8;
+          i4 += n8;
+         
+          //sumdiff(x[i3], x[i4], t1, t2); // {s, d}  <--| {a+b, a-b}
+          t1 = x[i3] + x[i4];
+          t2 = x[i3] - x[i4];
+         
+          t1 = -t1 * Math.SQRT1_2;
+          t2 *= Math.SQRT1_2;
+     
+          // sumdiff(t1, x[i2], x[i4], x[i3]); // {s, d}  <--| {a+b, a-b}
+          st1 = x[i2];
+          x[i4] = t1 + st1; 
+          x[i3] = t1 - st1;
+          
+          //sumdiff3(x[i1], t2, x[i2]); // {a, b, d} <--| {a+b, b, a-b}
+          x[i2] = x[i1] - t2;
+          x[i1] += t2;
+        }
+      } else {
+        for(i0 = ix; i0 < n; i0 += id) {
+          i1 = i0;
+          i2 = i1 + n4;
+          i3 = i2 + n4;
+          i4 = i3 + n4;
+     
+          //diffsum3_r(x[i3], x[i4], t1); // {a, b, s} <--| {a, b-a, a+b}
+          t1 = x[i3] + x[i4]; 
+          x[i4] -= x[i3];
+          
+          //sumdiff3(x[i1], t1, x[i3]);   // {a, b, d} <--| {a+b, b, a-b}
+          x[i3] = x[i1] - t1; 
+          x[i1] += t1;
         }
       }
-      width <<= 1
-    }
+   
+      ix = (id << 1) - n2;
+      id = id << 2;
+    } while (ix < n);
+ 
+    e = TWO_PI / n2;
 
-    return output
+    for (var j = 1; j < n8; j++) {
+      a = j * e;
+      ss1 = Math.sin(a);
+      cc1 = Math.cos(a);
+
+      //ss3 = sin(3*a); cc3 = cos(3*a);
+      cc3 = 4*cc1*(cc1*cc1-0.75);
+      ss3 = 4*ss1*(0.75-ss1*ss1);
+   
+      ix = 0; id = n2 << 1;
+      do {
+        for (i0 = ix; i0 < n; i0 += id) {
+          i1 = i0 + j;
+          i2 = i1 + n4;
+          i3 = i2 + n4;
+          i4 = i3 + n4;
+       
+          i5 = i0 + n4 - j;
+          i6 = i5 + n4;
+          i7 = i6 + n4;
+          i8 = i7 + n4;
+       
+          //cmult(c, s, x, y, &u, &v)
+          //cmult(cc1, ss1, x[i7], x[i3], t2, t1); // {u,v} <--| {x*c-y*s, x*s+y*c}
+          t2 = x[i7]*cc1 - x[i3]*ss1; 
+          t1 = x[i7]*ss1 + x[i3]*cc1;
+          
+          //cmult(cc3, ss3, x[i8], x[i4], t4, t3);
+          t4 = x[i8]*cc3 - x[i4]*ss3; 
+          t3 = x[i8]*ss3 + x[i4]*cc3;
+       
+          //sumdiff(t2, t4);   // {a, b} <--| {a+b, a-b}
+          st1 = t2 - t4;
+          t2 += t4;
+          t4 = st1;
+          
+          //sumdiff(t2, x[i6], x[i8], x[i3]); // {s, d}  <--| {a+b, a-b}
+          //st1 = x[i6]; x[i8] = t2 + st1; x[i3] = t2 - st1;
+          x[i8] = t2 + x[i6]; 
+          x[i3] = t2 - x[i6];
+         
+          //sumdiff_r(t1, t3); // {a, b} <--| {a+b, b-a}
+          st1 = t3 - t1;
+          t1 += t3;
+          t3 = st1;
+          
+          //sumdiff(t3, x[i2], x[i4], x[i7]); // {s, d}  <--| {a+b, a-b}
+          //st1 = x[i2]; x[i4] = t3 + st1; x[i7] = t3 - st1;
+          x[i4] = t3 + x[i2]; 
+          x[i7] = t3 - x[i2];
+         
+          //sumdiff3(x[i1], t1, x[i6]);   // {a, b, d} <--| {a+b, b, a-b}
+          x[i6] = x[i1] - t1; 
+          x[i1] += t1;
+          
+          //diffsum3_r(t4, x[i5], x[i2]); // {a, b, s} <--| {a, b-a, a+b}
+          x[i2] = t4 + x[i5]; 
+          x[i5] -= t4;
+        }
+     
+        ix = (id << 1) - n2;
+        id = id << 2;
+   
+      } while (ix < n);
+    }
   }
 
-  function BitReverseIndex(index, n) {
-    var bitreversed_index = 0
+  while (--i) {
+    rval = x[i];
+    ival = x[n-i-1];
+    mag = bSi * sqrt(rval * rval + ival * ival);
 
-    while (n > 1) {
-      bitreversed_index <<= 1
-      bitreversed_index += index & 1
-      index >>= 1
-      n >>= 1
-    }
-    return bitreversed_index
-  }
-
-  function BitReverseComplexArray(array) {
-    var n = array.length,
-        flips = {},
-        swap,
-        i
-
-    for(i = 0; i < n; i++) {
-      var r_i = BitReverseIndex(i, n)
-
-      if (flips.hasOwnProperty(i) || flips.hasOwnProperty(r_i)) continue
-
-      swap = array.real[r_i]
-      array.real[r_i] = array.real[i]
-      array.real[i] = swap
-
-      swap = array.imag[r_i]
-      array.imag[r_i] = array.imag[i]
-      array.imag[i] = swap
-
-      flips[i] = flips[r_i] = true
+    if (mag > this.peak) {
+      this.peakBand = i;
+      this.peak = mag;
     }
 
-    return array
+    spectrum[i] = mag;
   }
 
-  function LowestOddFactor(n) {
-    var factor = 3,
-        sqrt_n = sqrt(n)
+  spectrum[0] = bSi * x[0];
 
-    while(factor <= sqrt_n) {
-      if (n % factor === 0) return factor
-      factor = factor + 2
-    }
-    return n
-  }
-
-}(
-  typeof exports === 'undefined' && (this.fft = {}) || exports,
-  typeof require === 'undefined' && (this.complex_array) ||
-    require('./complex_array')
-)
+  return spectrum;
+};
